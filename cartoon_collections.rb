bcc5df08b4af38d03e931b7ee42003a7f9df4995
JC <bonassin@gmail.com>
2020-12-22 17:13:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(dwarf_names)
-dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
-roll_call_dwarves.each {|n| puts dwarf_names}
+def roll_call_dwarves(dwarves)
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+roll_call_dwarves.each {|n| puts dwarves}
 end
 
 def summon_captain_planet# code an argument here
